36ac2ae1a907726efc3169c28b7d504545eebec1
mnaithan <naithani.mayank@gmail.com>
2025-11-17 00:14:13 +0530

    environment setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = "ollama/llama3.1:8b"
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
